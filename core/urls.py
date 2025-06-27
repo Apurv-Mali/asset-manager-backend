755817debc6ed103658b50789b913d007ecbc83d
Apurv-Mali <maliapurav@gmail.com>
2025-06-27 21:06:27 +0530

    Add temporary superuser creation route

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from core.views import create_superuser
+from core.views import create_superuser_view
 
 urlpatterns = [
     path('users/', views.UserListView.as_view(), name='user-list'),
     path('delete-user/<int:user_id>/', views.DeleteUserView.as_view(), name='delete_user'),
-    path('createsuperuser/', create_superuser),
+    path('createsuperuser/', create_superuser_view),
 ]
